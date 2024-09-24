47cd8d2524cd42e64b836cb72fec4192e2a74b48
Vladislav_Kruzhilin <vl.cru@yandex.ru>
2024-09-24 19:34:01 +0300

    Initial commit

---
+++ Homework.py
@@ -0,0 +1,85 @@
+class Student:
+    def __init__(self, name, surname, gender):
+        self.name = name
+        self.surname = surname
+        self.gender = gender
+        self.finished_courses = []
+        self.courses_in_progress = []
+        self.grades = {}
+
+    def rate_hw(self, lecturer, course, grade):
+        if grade in range(11):
+            if isinstance(lecturer, Lecturer) and course in self.courses_in_progress and course in lecturer.courses_attached:
+                if course in lecturer.grades:
+                    lecturer.grades[course] += [grade]
+                else:
+                    lecturer.grades[course] = [grade]
+            else:
+                return "Ошибка"
+        else:
+            return "Ошибка"
+
+    def add_courses(self, course_name):
+        self.finished_courses.append(course_name)
+
+    def __str__(self):
+        return (f'Имя: {self.name}'
+                f'Фамилия: {self.surname}'
+                f' Средняя оценка за домащнее задание: {sum(Student.grades.values()) / len(self.grades)}'
+                f' Завершенные курсы: {self.finished_courses}')
+
+class Mentor:
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+        self.courses_attached = []
+
+
+class Lecturer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.courses_attached = []
+        self.grades = {}
+
+    def __str__(self):
+        return f'Имя: {self.name}\nФамилия: {self.surname}\n Средняя оценка за лекции: {float(sum(Lecturer.grades.values())) / len(self.grades)}'
+
+class Reviewer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.courses_attached = []
+
+    def __str__(self):
+        return f'Имя: {self.name}\nФамилия: {self.surname}'
+
+    def rate_hw(self, student, course, grade):
+        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+            if course in student.grades:
+                student.grades[course] += [grade]
+            else:
+                student.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
+best_student = Student('Ruoy', 'Eman', 'your_gender')
+best_student.courses_in_progress += ['Python']
+best_student.courses_in_progress += ['C++']
+
+cool_reviewer = Reviewer('Some', 'Buddy')
+cool_reviewer.courses_attached += ['Python']
+cool_reviewer.courses_attached += ['C++']
+
+
+cool_reviewer.rate_hw(best_student, 'Python', 25)
+cool_reviewer.rate_hw(best_student, 'Python', 10)
+cool_reviewer.rate_hw(best_student, 'C++', 10)
+
+cool_lecturer = Lecturer("Danil", "Dudkov")
+cool_lecturer.courses_attached += ['C++']
+cool_lecturer.courses_attached += ['Python']
+
+best_student.rate_hw(cool_lecturer, 'C++', 10)
+best_student.rate_hw(cool_lecturer, 'Python', 10)
+
+
+print(best_student.grades)
